be7dd68593fbb9aa7652c4ccd96d61bac9e58999
Phạm Xuân Hùng <huybeoku@gmail.com>
2025-10-10 10:22:42 +0700

    Add delete functionality for Current Transformer entity
    Implemented backend and frontend logic to delete Current Transformer entities, including related attachments, asset info, and associated CIM objects. Updated IPC and preload layers to expose the delete API, and integrated deletion into the tree navigation UI.

--- src/views/TreeNode/treeNavigation.vue
+++ src/views/TreeNode/treeNavigation.vue
@@ -2872,6 +2872,32 @@ export default {
                                 } else {
                                     this.$message.warning("Parent node not found in tree");
                                 }
+                            }else if (node.asset === 'Current transformer') {
+                                const entity = await window.electronAPI.getCurrentTransformerEntityByMrid(node.mrid, node.parentId);
+                                if (!entity.success) {
+                                    this.$message.error("Entity not found");
+                                    return;
+                                }
+                                console.log('Current transformer entity:', entity)
+                                const deleteSign = await window.electronAPI.deleteCurrentTransformerEntity(entity.data);
+                                console.log('Delete sign:', deleteSign)
+                                if (!deleteSign.success) {
+                                    this.$message.error("Delete data failed: " + (deleteSign.message || 'Unknown error'));
+                                    return;
+                                }
+                                // ✅ Xóa node khỏi cây organisationClientList
+                                const parentNode = this.findNodeById(node.parentId, this.organisationClientList);
+                                if (parentNode && Array.isArray(parentNode.children)) {
+                                    const index = parentNode.children.findIndex(child => child.mrid === node.mrid);
+                                    if (index !== -1) {
+                                        parentNode.children.splice(index, 1); // Xóa khỏi mảng children
+                                        this.$message.success("Delete data successfully");
+                                    } else {
+                                        this.$message.warning("Node not found in tree structure");
+                                    }
+                                } else {
+                                    this.$message.warning("Parent node not found in tree");
+                                }
                             }
                         }
                     } catch (error) {
