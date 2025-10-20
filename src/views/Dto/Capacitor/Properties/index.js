97a77da112900bea5036eb2d9c57ea77e58d842c
CNDung24 <dungcnhe181056@fpt.edu.vn>
2025-10-20 09:48:39 +0700

    create capacitor

---
+++ src/views/Dto/Capacitor/Properties/index.js
@@ -0,0 +1,16 @@
+class PropertiesDto {
+    constructor() {
+        this.mrid = '';
+        this.type = ''; // Kiểu thiết bị
+        this.kind = 'Capacitor';
+        this.serial_no = '';
+        this.manufacturer = '';
+        this.manufacturer_type = '';
+        this.manufacturing_year = '';
+        this.country_of_origin = '';
+        this.apparatus_id = '';
+        this.feeder = '';
+        this.comment = '';
+    }
+}
+export default PropertiesDto;
