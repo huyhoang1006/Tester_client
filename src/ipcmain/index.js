fbab10301d3124c4599bbe7bce93abce5441ab3a
unknown <huyhoang100697@gmail.com>
2025-12-19 11:46:41 +0700

    add app option

--- src/ipcmain/index.js
+++ src/ipcmain/index.js
@@ -1,5 +1,6 @@
 import * as ipcUploadCustom from './uploadCustom/index'
 import * as ipcCim from './cim/index.js'
 import * as ipcEntity from './entity/index.js'
+import * as ipcAppOption from './appOption/index'
 
-export { ipcCim, ipcEntity, ipcUploadCustom }
+export { ipcCim, ipcEntity, ipcUploadCustom, ipcAppOption }
--- src/preload/index.js
+++ src/preload/index.js
@@ -4,5 +4,6 @@ import * as uploadCustomPreload from './uploadCustomPreload'
 
 import * as entityPreload from './entity/index'
 import * as cimPreload from './cim/index'
+import * as appOptionPreload from './appOption/index'
 
-export { entityPreload, cimPreload, userPreload, importPreload, uploadCustomPreload }
+export { entityPreload, cimPreload, userPreload, importPreload, uploadCustomPreload, appOptionPreload }
