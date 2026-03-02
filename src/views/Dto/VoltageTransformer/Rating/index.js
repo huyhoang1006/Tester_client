3f24c2f79b59fc32e0aaa71fc9244d16f29f671b
DESKTOP-ARS116U\Hieu Trung <trunghieu999909@gmail.com>
2026-03-02 22:32:16 +0700

    fix: mapping VT to correct form

--- src/views/Dto/VoltageTransformer/Rating/index.js
+++ src/views/Dto/VoltageTransformer/Rating/index.js
@@ -3,17 +3,15 @@ import { UnitMultiplier } from "@/views/Enum/UnitMultiplier";
 
 class RatingDto {
     constructor() {
-        this.standard = { mrid: '', value: '', unit: 'string' },
-            this.rated_frequency_custom = '',
-            this.rated_frequency = { mrid: '', value: '', unit: UnitSymbol.Hz },
-            this.uprRatio = { mrid: '', value: '', unit: 'string' },
-            this.upr = '',
-            this.c1 = { mrid: '', value: '', unit: UnitMultiplier.k + '|' + UnitSymbol.F }
-        this.c2 = { mrid: '', value: '', unit: UnitMultiplier.k + '|' + UnitSymbol.F }
-        this.rated_voltage = { mrid: '', value: '', unit: UnitMultiplier.k + '|' + UnitSymbol.V }
+        this.standard = '';
+        this.rated_frequency_custom = '';
+        this.rated_frequency = { mrid: '', value: '', unit: UnitSymbol.Hz };
+        this.uprRatio = { mrid: '', value: '', unit: 'string' };
+        this.upr = '';
+        this.c1 = { mrid: '', value: '', unit: UnitMultiplier.k + '|' + UnitSymbol.F };
+        this.c2 = { mrid: '', value: '', unit: UnitMultiplier.k + '|' + UnitSymbol.F };
+        this.rated_voltage = { mrid: '', value: '', unit: UnitMultiplier.k + '|' + UnitSymbol.V };
     }
-
-
 }
 
 export default RatingDto;
