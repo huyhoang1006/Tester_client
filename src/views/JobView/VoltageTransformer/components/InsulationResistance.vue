0bb1f5fb78146a507beb25e36d853ef1b313293c
DESKTOP-ARS116U\Hieu Trung <trunghieu999909@gmail.com>
2026-03-09 17:37:48 +0700

    fix: job VT

--- src/views/JobView/VoltageTransformer/components/InsulationResistance.vue
+++ src/views/JobView/VoltageTransformer/components/InsulationResistance.vue
@@ -37,8 +37,7 @@
                 </tr>
             </thead>
             <tbody>
-                <template v-for="(item, index) in testData.table">
-                    <tr :key="index">
+                <tr v-for="(item, index) in testData.table.table1" :key="index">
                         <td>
                            {{ index + 1 }}
                         </td>
@@ -63,9 +62,14 @@
                             <span v-else-if="item.assessment.value === 'Fail'" class="fa-solid fa-xmark fail icon-status"></span>
                         </td>
                         <td>
-                            <el-input :class="nameColor(item.condition_indicator.value)" id="condition" type="text" size="mini" v-model="item.condition_indicator.value">
-                            </el-input>
-                        </td>
+                        <el-select :class="nameColor(item.condition_indicator.value)" id="condition" type="text"
+                            size="mini" v-model="item.condition_indicator.value">
+                            <el-option value="Good">Good</el-option>
+                            <el-option value="Fair">Fair</el-option>
+                            <el-option value="Poor">Poor</el-option>
+                            <el-option value="Bad">Bad</el-option>
+                        </el-select>
+                    </td>
                         <td>
                             <el-button size="mini" type="primary" class="w-100" @click="addTest(index)">
                                 <i class="fa-solid fa-plus"></i>
@@ -76,8 +80,7 @@
                                 <i class="fas fa-trash"></i>
                             </el-button>
                         </td>
-                    </tr>
-                </template>
+                </tr>
             </tbody>
         </table>
 
@@ -92,6 +95,9 @@
 </template>
 
 <script>
+import voltageTransformerTestMap from '@/config/test-definitions/VoltageTransformer'
+import * as common from '../../Common/index'
+
 export default {
     name :"InsulationResistance",
     data() {
@@ -99,6 +105,12 @@ export default {
             openAssessmentDialog: false,
             openConditionIndicatorDialog: false,
         }
+    },
+    mounted() {
+        // Initialize table if needed
+        this.$nextTick(() => {
+            this.initializeTable()
+        })
     },
     props: {
         data: {
@@ -117,44 +129,52 @@ export default {
         assetData() {
             return this.asset
         },
+        rowData() {
+            return common.buildEmptyTestRow(voltageTransformerTestMap['InsulationResistance'].columns)
+        }
     },
     watch: {
+        'testData.table': {
+            immediate: true,
+            handler: function (newVal) {
+                // Convert array to object if needed (for backward compatibility)
+                if (newVal && Array.isArray(newVal)) {
+                    const tableObject = { table1: newVal }
+                    this.$set(this.testData, 'table', tableObject)
+                    return
+                }
+                
+                // Initialize table if empty
+                if (!newVal || (typeof newVal === 'object' && Object.keys(newVal).length === 0)) {
+                    this.$nextTick(() => {
+                        this.initializeTable()
+                    })
+                }
+            }
+        }
     },
     methods: {
+        initializeTable() {
+            if (!this.testData.table) {
+                this.$set(this.testData, 'table', {})
+            }
+            
+            if (Object.keys(this.testData.table).length === 0) {
+                this.$set(this.testData.table, 'table1', [])
+            }
+            
+            // Ensure table1 exists
+            if (!this.testData.table.table1) {
+                this.$set(this.testData.table, 'table1', [])
+            }
+        },
         add() {
-            this.testData.table.push({
-                mrid : "",
-                measurement : {
-                    mrid : "",
-                    value : "",
-                    unit : "",
-                    type : "string"
-                },
-                test_voltage : {
-                    mrid : "",
-                    value : "",
-                    unit : "V",
-                    type : "analog"
-                },
-                r60s : {
-                    mrid : "",
-                    value : "",
-                    unit : "M|Ω",
-                    type : "analog"
-                },
-                assessment : {
-                    mrid : "",
-                    value : "",
-                    unit : "",
-                    type : "discrete"
-                },
-                condition_indicator : {
-                    mrid : "",
-                    value : "",
-                    unit : "",
-                    type : "discrete"
-                }
-            })
+            if (!this.testData.table.table1) {
+                this.initializeTable()
+            }
+            this.testData.table.table1.push(
+                JSON.parse(JSON.stringify(this.rowData))
+            )
         },
         removeAll() {
             this.$confirm('This will delete the file. Continue?', 'Warning', {
@@ -163,60 +183,33 @@ export default {
                     type: 'warning'
                 })
                 .then( () => {
-                    this.testData.table = []
+                    this.testData.table.table1 = []
                 }
             )
         },
         deleteTest(index) {
-            this.testData.table.splice(index, 1)
+            this.testData.table.table1.splice(index, 1)
         },
         addTest(index) {
-            const data = {
-                mrid : "",
-                measurement : {
-                    mrid : "",
-                    value : "",
-                    unit : "",
-                    type : "string"
-                },
-                test_voltage : {
-                    mrid : "",
-                    value : "",
-                    unit : "V",
-                    type : "analog"
-                },
-                r60s : {
-                    mrid : "",
-                    value : "",
-                    unit : "M|Ω",
-                    type : "analog"
-                },
-                assessment : {
-                    mrid : "",
-                    value : "",
-                    unit : "",
-                    type : "discrete"
-                },
-                condition_indicator : {
-                    mrid : "",
-                    value : "",
-                    unit : "",
-                    type : "discrete"
-                }
-            }
-            this.testData.table.splice(index+1, 0, data)
+            const data = JSON.parse(JSON.stringify(this.rowData))
+            this.testData.table.table1.splice(index+1, 0, data)
         },
         calculator() {
             this.$message.success('Calculating successfully')
         },
 
         clear() {
-            this.testData.table.forEach((element) => {
-                element.measurement.value = "",
-                element.test_voltage.value = '',
-                element.r60s.value = '',
-                element.assessment.value = '',
-                element.condition_indicator.value = ''
+            if (!this.testData.table.table1) {
+                this.initializeTable()
+                return
+            }
+            this.testData.table.table1.forEach(row => {
+                Object.keys(row).forEach(key => {
+                    if (key === "mrid") return;
+                    if (row[key] && typeof row[key] === "object" && "value" in row[key]) {
+                        row[key].value = ""
+                    }
+                })
             })
         },
         nameColor(data) {
