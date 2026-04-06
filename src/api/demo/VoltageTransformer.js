850aa765bd642a5fbb2c8be3363bf19fc3cb0bf8
bndat615 <bndat.615@gmail.com>
2026-04-06 09:31:36 +0700

    Fix load IP Address to call API

--- src/api/demo/VoltageTransformer.js
+++ src/api/demo/VoltageTransformer.js
@@ -3,5 +3,5 @@ import client from '@/utils/client'
 import constant from '@/utils/constant'
 const prefix = 'voltage-transformer'
 export const getVoltageTransformerById = (id) => {
-    return client.get(`${constant.API_BASE_URL}${prefix}/${id}`)
+    return client.get(`/${prefix}/${id}`)
 }
